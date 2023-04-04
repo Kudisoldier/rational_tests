ee4bd2355a2b5b743de5e1c92e530d8600699c17
Efim Lyubichev <efim.lyubichev@vk.team>
2023-04-04 16:48:42 +0300

    fix pom and refactor parametrized tests

--- src/test/java/ParameterizedTestLess.java
+++ src/test/java/ParameterizedTestLess.java
@@ -4,13 +4,14 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(value = Parameterized.class)
-public class ParameterizedTestConstructor {
+public class ParameterizedTestLess {
     private Rational number1;
     private Rational number2;
 
-    public ParameterizedTestConstructor(Rational number1, Rational number2) {
+    public ParameterizedTestLess(Rational number1, Rational number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
@@ -28,19 +29,12 @@ public class ParameterizedTestConstructor {
     @Parameterized.Parameters(name = "Case {index}: {0} = {1}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {new Rational(1, 2), new Rational(1, 2)},
-                {new Rational(2, 4), new Rational(1, 2)},
-                {new Rational(6, 2), new Rational(3, 1)},
-                {new Rational(0, 2), new Rational(0, 1)},
-                {new Rational(-5, -2), new Rational(5, 2)},
-                {new Rational(1, -7), new Rational(-1, 7)},
-                {new Rational(17, 17), new Rational(1, 1)}
+                {new Rational(1, 2), new Rational(1, 2)}
         });
     }
 
     @Test
     public void constructor() {
-        assertEquals("Standard constructor returns wrong numerator", number1.getNumerator(), number2.getNumerator());
-        assertEquals("Standard constructor returns wrong denominator", number1.getDenominator(), number2.getDenominator());
+        assertTrue(number1.toString() + " is not less " + number2.toString(), number1.less(number2));
     }
 }
